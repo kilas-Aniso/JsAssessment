cb33b312ba5d9ae08865669d0c54e3b51824e092
kilas-aniso <kilasaniso@gmail.com>
2023-05-01 20:03:11 +0300

    changes

--- assessment2.js
+++ assessment2.js
@@ -21,38 +21,46 @@ console.log(x)
 
 // Create a function getBooksByAuthor that takes an author's name as an argument and
 // returns an array of all books by that author.
-function getBooksByAuthor(author){
-  let m = books.filter(books=>books.author)
-return m
-
+function getBooksByAuthor(authorName){
+     return books.filter(book=>book.author===authorName)
 }
-m = getBooksByAuthor("Charles Dickens")
-console.log(m)
+console.log(getBooksByAuthor("Homer"))
 // Create a function addNewBook that takes a book object as an argument and adds it
 // to the library, ensuring that the new book has all required properties (title, author,
     // publicationYear, and isAvailable).
 function addNewBook(newBook){
-
-   books.title=newBook("Born a crime");
-   books.author=newBook
-
+const properties =['title','author','publicationYear','isAvailable']
+for (let i=0;i<properties.length;i++){
+    const eachProperty=properties[i]
+    if(!newBook.hasOwnProperty(eachProperty)){
+      return "book does not have required property"
+    }
+    else{
+        books.push(newBook)
+        return books
+    }
+    
 }
+}
+console.log(addNewBook({"title":"hello", "author":"cever", "publicationYear":2022, "isAvailable": true}))
+
 
 
 // Create a function checkoutBook that takes a book title as an argument and changes
 // the book's isAvailable property to false. If the book is not found in the library, the
 // function should return a message indicating that the book is not available.
-function checkoutBook(title){
-    const catogory1=title[i]
-    for (let i =0;i<=books.length;i++){
-        if(catogory1!=title[i]){
-            return ("x")
-        }
-        else{
-            return ("book is not available")
-        }
-    }
+function checkoutBook(booktitle){
+ let book = books.find(book=>book.title===booktitle)
+ if (book){
+    book.isAvailable=false
+    return true
+ }
+else{
+    return false
 }
+}
+const check=checkoutBook('Great Expectations');
+console.log(check)
 
 
 
@@ -60,6 +68,19 @@ function checkoutBook(title){
 // book's isAvailable property to true. If the book is not found in the library, the function
 // should return a message indicating that the book does not belong to the library.
 
-function returnBook(){
-
+function returnBook(titlebook){
+let book =books.find(book=>book.title==titlebook)
+if (book){
+    book.isAvailable = true
+    return "Great Expectations is available now"
 }
+else{
+    return "book does not belong here"
+}
+}
+
+const returnedbooks=returnBook('Great Expectations')
+console.log(returnedbooks)
+
+
+
